4b6bc147ed850dfb892bb15f25707bf4be422681
LeeAntonV <koshkai2005@gmail.com>
2023-10-29 21:01:40 +0100

    first commit

---
+++ cmd/internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"github.com/cristalhq/aconfig"
+	"github.com/cristalhq/aconfig/aconfighcl"
+	"log"
+	"sync"
+
+	_ "github.com/cristalhq/aconfig"
+	_ "github.com/cristalhq/aconfig/aconfighcl"
+)
+
+type Config struct {
+	DatabaseDSN string `hcl:"database_dsn" env:"DatabaseDSN"`
+}
+
+var (
+	cfg  Config
+	once sync.Once
+)
+
+func Get() Config {
+	once.Do(func() {
+		loader := aconfig.LoaderFor(&cfg, aconfig.Config{
+			EnvPrefix: "NFB",
+			Files:     []string{"./config.local.hcl"},
+			FileDecoders: map[string]aconfig.FileDecoder{
+				".hcl": aconfighcl.New(),
+			},
+		})
+
+		if err := loader.Load(); err != nil {
+			log.Printf("[ERROR] failed to load config: %v", err)
+		}
+	})
+
+	return cfg
+}
